533d9478f984974be8e735855043225ddb352e95
Mili <milagrosfarfan2020@gmail.com>
2020-04-10 23:55:22 -0300

    Other minimal change

--- test.pas
+++ test.pas
@@ -2,9 +2,9 @@ Program programa;
 var
   num1, num2 : integer;
 begin
-  write('Ingrese el primer numero: ');
+  write('Ingrese el primer nùmero: ');
   readln(num1);
-  write('Ingrese el segundo numero: ');
+  write('Ingrese el segundo nùmero: ');
   readln(num2);
   writeln('La suma entre num1 y num2 es: ', num1+num2);
   writeln('Presionar cualquier tecla para cerrar el programa ...');
