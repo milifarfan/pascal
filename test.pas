cf36f00079cb3dbfe00598be58cc293ec41cfd28
Mili <milagrosfarfan2020@gmail.com>
2020-04-10 23:42:10 -0300

    First commit

---
+++ test.pas
@@ -0,0 +1,12 @@
+Program programa;
+var
+  num1, num2 : integer;
+begin
+  write('Ingrese el primer numero: ');
+  readln(num1);
+  write('Ingrese el segundo numero: ');
+  readln(num2);
+  writeln('La suma entre num1 y num2 es: ', num1+num2);
+  writeln('Presionar cualquier tecla para cerrar el programa...');
+  readln;
+end.
